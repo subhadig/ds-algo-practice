13694cba2ae95dc633cfcdb5da9874d707292926
Subhadip Ghosh <subhadipdipghosh11@gmail.com>
2020-10-30 23:35:00 +0530

    updated

--- java/ImplementStrStr.java
+++ java/ImplementStrStr.java
@@ -1,32 +1,54 @@
 // https://leetcode.com/explore/learn/card/array-and-string/203/introduction-to-string/1161/
 
-// In progres, failing for input: "mississippi", "issip"
 class Solution {
     public int strStr(String haystack, String needle) {
         if("".equals(needle)) {
             return 0;
+        } else if(haystack.length() == needle.length() && !haystack.equals(needle)) {
+            return -1;
         }
         
-        int index = -1, ni = 0;
-        boolean matchstart = false;
+        List<Index> indexes = new ArrayList<>();
         
         for(int i = 0; i < haystack.length(); i++) {
-            if(haystack.charAt(i) == needle.charAt(ni)) {
-                if(ni == 0) {
-                    index = i;
+            
+            Iterator<Index> itr = indexes.iterator();
+            while(itr.hasNext()) {
+                Index ind = itr.next();
+                if(haystack.charAt(i) == needle.charAt(ind.ni)) {
+                    ind.ni++;
+                    if(ind.ni == needle.length()) {
+                        return ind.hi;
+                    }
+                } else {
+                    itr.remove();
                 }
-                ni++;
-                if(ni == needle.length()) {
-                    return index;
+            }
+            
+            if(haystack.charAt(i) == needle.charAt(0)) {
+                if(needle.length() == 1) {
+                    return i;
                 }
-                matchstart = true;
-            } else {
-                ni = 0;
-                index = -1;
-                matchstart = false;
+                indexes.add(new Index(i));
+            }
+        }
+        
+        for(Index ind : indexes) {
+            if(ind.ni == needle.length()) {
+                return ind.hi;
             }
         }
         
         return -1;
+    }
+    
+    static class Index {
+        int hi = -1;
+        int ni = 0;
+        
+        Index(int hi) {
+            this.hi = hi;
+            ni++;
+        }
     }
 }
